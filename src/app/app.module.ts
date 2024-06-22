52e70305673b8fe7b8579d1d10667694c9d61e4f
rifaa oumnia <rifaaoumnia@gmail.com>
2024-06-23 00:14:58 +0100

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { CustomerAccountsComponent } from './customer-accounts/customer-accounts.component';
+
+@NgModule({ declarations: [
+        AppComponent,
+        NavbarComponent,
+        CustomersComponent,
+        AccountsComponent,
+        NewCustomerComponent,
+        CustomerAccountsComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+export class AppModule { }
